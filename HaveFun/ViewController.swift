323388303c02ee84ff75aefd26ea8d7c06496845
Cinthia Gisela Camacho <gis25cam@gmail.com>
2022-07-14 13:24:07 -0500

    add login

--- HaveFun/ViewController.swift
+++ HaveFun/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+// MARK: ViewController class
+final class ViewController: UIViewController {
+    
+  // MARK: - Properties
+    private weak var userTextField: UITextField?
+    private weak var passwordTextField: UITextField?
+    
+  // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        initializeButtons()
+        setUpConstrains()
     }
-
-
+    
+  // MARK: - Private methods
+    private func initializeButtons() {
+      let userTextField = UITextField(frame: .zero)
+        userTextField.backgroundColor = .blue
+      userTextField.isUserInteractionEnabled = true
+      userTextField.translatesAutoresizingMaskIntoConstraints = false
+      userTextField.layer.cornerRadius = 7
+      userTextField.placeholder = "User"
+      userTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+      userTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 20))
+      userTextField.leftViewMode = UITextField.ViewMode.always
+      view.addSubview(userTextField)
+      self.userTextField = userTextField
+      let passwordTextField = UITextField(frame: .zero)
+        passwordTextField.backgroundColor = .blue
+      passwordTextField.isUserInteractionEnabled = true
+      passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+      passwordTextField.layer.cornerRadius = 7
+      passwordTextField.placeholder = "Password"
+      passwordTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+      passwordTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 20))
+      passwordTextField.leftViewMode = UITextField.ViewMode.always
+      view.addSubview(passwordTextField)
+      self.passwordTextField = passwordTextField
+    }
+    private func setUpConstrains() {
+      guard let userTextField = userTextField, let passwordTextField = passwordTextField else { return }
+      NSLayoutConstraint.activate([
+        userTextField.heightAnchor.constraint(equalToConstant: 50),
+        passwordTextField.heightAnchor.constraint(equalToConstant: 50),
+        userTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: UIScreen.main.bounds.height/3),
+        passwordTextField.topAnchor.constraint(equalTo: userTextField.bottomAnchor, constant: 40),
+        userTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+        passwordTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+        userTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+        passwordTextField.centerXAnchor.constraint(
+              equalTo: view.centerXAnchor, constant: 0)
+      ])
+    }
 }
-
